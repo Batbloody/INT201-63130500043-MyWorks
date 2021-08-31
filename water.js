cedb9661b9b68dc8519cc47c977632be8673eb90
Batbloody <68815336+Batbloody@users.noreply.github.com>
2021-08-31 11:13:06 +0700

    water

---
+++ water.js
@@ -0,0 +1,8 @@
+let WaterDispenser = {
+    name: ['coke', 'pepsi'],
+    price: [15, 18]
+};
+
+console.log(WaterDispenser);
+console.log(WaterDispenser.name[0]);
+console.log(WaterDispenser.name[WaterDispenser.name.length-1]);
