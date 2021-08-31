c903f1ff720441946979e889536f7413a0b86b07
Batbloody <68815336+Batbloody@users.noreply.github.com>
2021-08-31 11:29:42 +0700

    water2

--- water.js
+++ water.js
@@ -1,8 +1,15 @@
-let WaterDispenser = {
-    name: ['coke', 'pepsi'],
-    price: [15, 18]
-};
+// let WaterDispenser = {
+//     name: ['coke', 'pepsi'],
+//     price: [15, 18]
+// };
 
-console.log(WaterDispenser);
-console.log(WaterDispenser.name[0]);
-console.log(WaterDispenser.name[WaterDispenser.name.length-1]);
+// console.log(WaterDispenser);
+// console.log(WaterDispenser.name[0]);
+// console.log(WaterDispenser.name[WaterDispenser.name.length-1]);
+
+let product1 = {"name" : 'coke'  , "price" : 15 } ;
+let product2 = {"name" : 'pepsi'  , "price" : 18 } ;
+
+let waterDispenser = [product1 , product2 ];
+
+for(let water of waterDispenser) {console.log(water)} ;
