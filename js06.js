45920e0d010ba2d7a0900d2d7dc07cbe37fde302
Batbloody <68815336+Batbloody@users.noreply.github.com>
2021-09-14 10:21:37 +0700

    Update js06.js

--- js06.js
+++ js06.js
@@ -1,4 +1,10 @@
+//object assignment example
+
 let per1 = { id: 1, name: 'Joe' };
+
+//per1 is an object variable,
+
+//store memory address that points to object properties
 
 let per2 = per1;
 
@@ -21,3 +27,25 @@ console.log(per1);
 per2.id = 3;
 
 console.log(per1);
+
+
+
+//primitive assignment example
+
+let num1 = 10; //num1 is  a primitive variable, store value 10
+
+let num2 = num1; //num2=10
+
+doSomething(num2);
+
+function doSomething(num) {
+
+  //num=num2=10
+
+  num = 20; //num=20
+
+}
+
+console.log(num1); //num1=10
+
+console.log(num2); //num2=10
