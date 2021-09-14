b5f9a409cc9c4fa5d32d11079f0d7c9cc778aabf
Batbloody <68815336+Batbloody@users.noreply.github.com>
2021-09-14 11:48:15 +0700

    Update js06.js

--- js06.js
+++ js06.js
@@ -49,3 +49,26 @@ function doSomething(num) {
 console.log(num1); //num1=10
 
 console.log(num2); //num2=10
+
+
+
+
+//array
+
+let arr1 = [];
+
+console.log(typeof arr1);
+
+
+
+arr1 = [5, 2, 10, 'student']; //untyped & unsized
+
+console.log(arr1.length);
+
+console.log(arr1[0]);
+
+console.log(arr1[arr1.length - 1]);
+
+
+
+console.log(   arr1[     arr1[1]       ]     );
