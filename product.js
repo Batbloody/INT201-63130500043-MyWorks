1c4744106af6fa42362c6398643cf32b9f70303c
Batbloody <68815336+Batbloody@users.noreply.github.com>
2021-10-26 12:09:31 +0700

    11

--- product.js
+++ product.js
@@ -1,30 +1,30 @@
 let stationery = [
     {
-        id : 01,
+        id : 1,
         name : "pencil",
         stock : 30 ,
         price : 5
     },
     {
-        id : 02,
+        id : 2,
         name : "pen",
         stock : 60 ,
         price : 10
     },
     {
-        id : 03,
+        id : 3,
         name : "notebook",
         stock : 10 ,
         price : 15
     },
     {
-        id : 04,
+        id : 4,
         name : "eraser",
         stock : 20 ,
         price : 5
     },
     {
-        id : 05,
+        id : 5,
         name : "Paint brush",
         stock : 25 ,
         price : 20
